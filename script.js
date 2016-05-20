0236fe311c7e2f41d753700eab4291f5c74b54fe
Agilityforce <rikki.lem@gmail.com>
2016-05-20 21:21:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var canvas, ctx, launch;
 var fps = 60;
+var counter = 1;
+var balls = [];
 window.onload = function(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
@@ -8,19 +10,55 @@ window.onload = function(){
     launch.style.cursor = "pointer";
     loop();
     launch.onclick = function(){
-        random();
+        balls.push(new Ball());
+        balls[counter - 1].random();
+        counter++;
     }
 }
 
 var width = 800;
 var height = 600;
 
-var color = "orange";
-var size, x, y;
-var vx, vy;
-var gravity = 1;
-var resistance = 1; //set values from 0 to 10 to see results
-var bounceFactor = 0.9;
+function Ball(){
+    this.color = this.randomColor();
+    this.size;
+    this.x;
+    this.y;
+    this.vx;
+    this.vy;
+    this.gravity = 1;
+    this.resistance = 1;
+    this.bounceFactor = 0.9;
+}
+
+Ball.prototype.update = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+    
+    this.vx *= (2000 - this.resistance * 10) / 2000;
+    this.x += this.vx;
+    this.vy += this.gravity;
+    this.y += this.vy;
+    
+    if(this.x >= width - this.size){
+        this.x = width - this.size;
+        this.vx = -this.vx;
+    }else if(this.x <= this.size){
+        this.x = this.size;
+        this.vx = -this.vx;
+    }else if(this.vx > -0.01 && this.vx < 0.01){
+        this.vx = 0;
+    }
+    
+    if(this.y >= height - this.size){
+        this.y = height - this.size;
+        this.vy *= -this.bounceFactor;
+        if(this.bounceFactor === 1) this.y += this.vy;
+    }
+};
 
 function clearCanvas(){
     ctx.fillStyle = "#000";
@@ -29,48 +67,32 @@ function clearCanvas(){
 }
 
 function loop(){
-    setTimeout(function(){
+    setInterval(function(){
         clearCanvas();
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.arc(x, y, size, 0, 2*Math.PI, false);
-        ctx.fill();
-        ctx.closePath();
         
-        vx *= (2000 - resistance * 10) / 2000;
-        x += vx;
-        vy += gravity;
-        y += vy;
-        
-        if(x >= width - size){
-            x = width - size;
-            vx = -vx;
-        }else if(x <= size){
-            x = size;
-            vx = -vx;
-        }else if(vx > -0.01 && vx < 0.01){
-            vx = 0;
+        for(i = 0; i < counter; i++){
+            balls[i].update();
         }
-        
-        if(y >= height - size){
-            y = height - size;
-            vy *= -bounceFactor;
-            if(bounceFactor === 1) y += vy;
-        }
-        
         loop();
     }, 1000/fps);
 }
 
-function random(){
-    size = Math.floor((Math.random() * 50) + 10);
-    x = Math.floor((Math.random() * (width - 2*size)) + size);
-    y = Math.floor((Math.random() * (height - 2*size)) + size);
+Ball.prototype.random = function(){
+    this.size = Math.floor((Math.random() * 50) + 10);
+    this.x = Math.floor((Math.random() * (width - 2*this.size)) + this.size);
+    this.y = Math.floor((Math.random() * (height - 2*this.size)) + this.size);
     
     randX = Math.random();
     randY = Math.random();
-    vx = Math.floor((Math.random() * 30) + 1);
-    vy = Math.floor((Math.random() * 30) + 1);
-    if(randX < 0.5) vx = -vx;
-    if(randY < 0.5) vy = -vy;
-}
+    this.vx = Math.floor((Math.random() * 30) + 1);
+    this.vy = Math.floor((Math.random() * 30) + 1);
+    if(randX < 0.5) this.vx = -this.vx;
+    if(randY < 0.5) this.vy = -this.vy;
+};
+
+Ball.prototype.randomColor = function(){
+    var r = Math.floor((Math.random() * 255) + 1);
+    var g = Math.floor((Math.random() * 255) + 1);
+    var b = Math.floor((Math.random() * 255) + 1);
+    return this.color = "rgb(" + r +"," + g + "," + b + ")";
+};
